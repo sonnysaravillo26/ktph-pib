c7cf82d31b67c6367144395d1df887b6dbd9df2e
SunnyDay <4044909+sonnysaravillo26@users.noreply.github.com>
2023-12-11 00:24:28 +0800

    header updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,42 @@
 // Header.js
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
-      <h1>Your Animated Single Page</h1>
+      <div className="header-bed header-content-mid">
+        <div className="header-bed-content">
+          <h1 className="bed-title">BED</h1>
+          <h1 className="bed-number">34</h1>
+        </div>
+      </div>
+
+      <div className="header-name">
+        <h1 className="header-patient-name header-content-mid2">
+          Saravillo Sonny Barrientos
+        </h1>
+        <h1 className="header-patient-location">
+          <span class="header-patient-location-icon"> icon </span>
+          <span class="header-patient-location-text"> Admission Block </span>
+        </h1>
+      </div>
+
+      <div className="header-icons">
+        {/* maximum of 4 icons, justify right */} 
+          <h1 className="header-this-icon header-content-mid2">
+            <span class="header-icon-view"> 1 </span>
+          </h1>  
+          <h1 className="header-this-icon header-content-mid2">
+            <span class="header-icon-view"> 2 </span>
+          </h1>  
+          <h1 className="header-this-icon header-content-mid2">
+            <span class="header-icon-view"> 3 </span>
+          </h1>  
+          <h1 className="header-this-icon header-content-mid2">
+            <span class="header-icon-view"> 4 </span>
+          </h1>  
+      </div>
     </header>
   );
 }
